f1bbca8746ab98c8bfaec0f43e173fd201c072e8
Prince Yadav <pkryadav9304@gmail.com>
2023-10-07 17:09:36 +0530

    Code Added To Check Prime

---
+++ CheckPrime.js
@@ -0,0 +1,14 @@
+let N = 13
+count = 0
+
+for(i=2; i<=N; i++){
+    if (i%N==0){
+        count += 1
+    }
+}
+if (count == 2){
+    console.log("It Is A Prime Number")
+}
+else{
+    console.log("It Is Not A Prime Number")
+}
